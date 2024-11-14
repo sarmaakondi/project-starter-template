56f645d9dc05ca3df9f167e7fee85c8790a60716
sarmaakondi <avnmsarma@gmail.com>
2024-11-14 18:06:55 +1100

    Add compression algorithm and minify setup in vite config file

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import viteCompression from "vite-plugin-compression";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+    plugins: [
+        react(),
+        viteCompression({
+            algorithm: "brotliCompress",
+            threshold: 1025,
+            verbose: true,
+        }),
+    ],
+    build: {
+        minify: "terser",
+        terserOptions: { compress: { drop_console: true } },
+    },
+});
